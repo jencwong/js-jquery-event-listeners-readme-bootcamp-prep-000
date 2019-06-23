1221a4d36994100699f9ce4d0fedbbba227adf97
Jen W <jsc525@gmail.com>
2019-06-23 11:49:17 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -13,8 +13,8 @@ function frameIt() {
 
 function pressIt() {
   $('input').on("keydown", function() {
-    if(key.which == 71){
-    alert()
+    if(key.which == 71) {
+    alert("you press G")
   }
 }
 
