6cb4e9e85d73ea6f2ae924269156f64290f35378
Jen W <jsc525@gmail.com>
2019-06-23 11:21:27 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
 
 // call functions here
-$('p').on("click", function getIt(alert("Hey!"))); 
-  
-  
+$("p").on("click", function getIt() {
+  alert("Hey!"); 
+  }); 
 });
